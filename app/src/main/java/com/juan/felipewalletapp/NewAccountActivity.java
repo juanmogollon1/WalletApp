128ee1a4d59e55787b367b1b3dc9064133188666
just <juanfelipecabanzo041@gmail.com>
2023-09-06 17:54:51 -0500

    adelanto miercoles

--- app/src/main/java/com/juan/felipewalletapp/NewAccountActivity.java
+++ app/src/main/java/com/juan/felipewalletapp/NewAccountActivity.java
@@ -2,13 +2,27 @@ package com.juan.felipewalletapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class NewAccountActivity extends AppCompatActivity {
+
+    private ImageView ivBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
+        ivBack = findViewById(R.id.iv_back);
+
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntet = new Intent(NewAccountActivity.this,MainActivity.class);
+                startActivity(myIntet);
+            }
+        });
     }
 }
